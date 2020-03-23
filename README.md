15649155cf49ad91668098e61d83626d15460b42
LaurineGillet <laurinenet@gmail.com>
2020-03-23 14:17:08 +0100

    exo3

---
+++ README.md
@@ -0,0 +1,5 @@
+exo 2 :
+python -m jelag
+
+exo 3 :
+python exo3.py
---
+++ exo3/exo3.py
@@ -0,0 +1,10 @@
+a = '408'
+print("type a", type(a))
+b = 9
+print("type b", type(b))
+a = int(a)
+print("type a", type(a))
+c = a / b
+print("type c", type(c))
+
+input()
