b4bfeda7853b727ac6f313dc292943e25dc4b162
LaurineGillet <laurinenet@gmail.com>
2020-03-24 10:20:23 +0100

    exo8

---
+++ exo8/superexercice.py
@@ -0,0 +1,8 @@
+class SuperExercice(One, Two):
+    EXERCICE = 'dev'
+    _techno = 'Python'
+
+    def __init__(self):
+
+    def exercer(self):
+        print("Coucou l'aformac je fais mes modules")
