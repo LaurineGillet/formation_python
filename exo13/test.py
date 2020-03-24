e4952db9baae637d1c3839de86e78612092d7832
LaurineGillet <laurinenet@gmail.com>
2020-03-24 16:26:42 +0100

    exo14

---
+++ exo13/test.py
@@ -0,0 +1,6 @@
+from exo13 import MyClass
+
+m = MyClass
+print(m.pq)
+m.pq = 50
+print(m.pq)
---
+++ exo14/exo14.py
@@ -0,0 +1,19 @@
+class Class:
+    """[summary]
+    
+    Returns:
+        [type] -- [description]
+    """
+    @staticmethod
+    def staticmethod(a):
+        """[summary]
+        
+        Arguments:
+            a {[type]} -- [description]
+        
+        Returns:
+            [type] -- [description]
+        """
+        return a.upper()
+
+print(Class.staticmethod("jojo l'astico"))
